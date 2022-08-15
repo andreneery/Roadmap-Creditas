6e6cc83107787bc21fa678ab824b220474f226ee
André Nery <andre.nery@creditas.com>
2022-08-15 19:37:14 -0300

    adcionando exercicio do retangulo

--- src/main/kotlin/calculoRetanguloOOP/Retangulo.kt
+++ src/main/kotlin/calculoRetanguloOOP/Retangulo.kt
@@ -1,4 +1,17 @@
 package calculoRetanguloOOP
 
-class Retangulo {
+class Retangulo(
+    var width: Int,
+    var height: Int,
+    var color: String
+){
+
+
+    fun calcArea(): Int{
+        return width * height
+    }
+
+    override fun toString(): String{
+        return "Width = ${width},\nHeight = ${height},\nColor = ${color}, \nArea: ${calcArea()}"
+    }
 }
--- src/main/kotlin/calculoRetanguloOOP/main.kt
+++ src/main/kotlin/calculoRetanguloOOP/main.kt
@@ -1,2 +1,16 @@
 package calculoRetanguloOOP
 
+fun main(){
+
+    val retangulo = Retangulo(
+        width = 5,
+        height = 15,
+        color = "red"
+    )
+
+    var resultado = retangulo.toString().also {
+        print(it)
+    }
+
+
+}
