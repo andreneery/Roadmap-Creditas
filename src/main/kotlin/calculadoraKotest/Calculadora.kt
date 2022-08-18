b1d2f2e5d97c06be97e31b37072b536ebda938e8
André Nery <andre.nery@creditas.com>
2022-08-18 10:43:42 -0300

    adding files

---
+++ src/main/kotlin/calculadoraKotest/Calculadora.kt
@@ -0,0 +1,20 @@
+package calculadoraKotest
+
+class Calculadora {
+
+    fun soma(x: Int, y: Int): Int {
+        return x + y
+    }
+
+    fun subtração(x: Int, y: Int): Int {
+        return x - y
+    }
+
+    fun multiplicação(x: Int, y: Int): Int {
+        return x * y
+    }
+
+    fun divisão(x: Int, y: Int): Int {
+        return x / y
+    }
+}
