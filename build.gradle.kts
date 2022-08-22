57a599b6480ec973bc2a4cb34594d6a5c15711e1
André Nery <andre.nery@creditas.com>
2022-08-22 09:45:46 -0300

    config build

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("junit:junit:4.13.1")
+    implementation("junit:junit:4.13.1")
+    implementation("junit:junit:4.13.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.testng:testng:7.1.0")
     testImplementation(kotlin("test"))
 }
 
