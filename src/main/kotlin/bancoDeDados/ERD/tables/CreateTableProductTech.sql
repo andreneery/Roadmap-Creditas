d86c445188f146be123f4e22745798809ef07cc1
André Nery <andre.nery@creditas.com>
2023-02-27 09:59:01 -0300

    criando database, alterando tempo_empresa para data_contratacao e alterando tipo de assistiu

--- src/main/kotlin/bancoDeDados/ERD/tables/CreateTableProductTech.sql
+++ src/main/kotlin/bancoDeDados/ERD/tables/CreateTableProductTech.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS TRIBE_EXERCISE;
+
+USE TRIBE_EXERCISE;
+
 CREATE TABLE product_tech(
     tribo_name varchar(50) not null,
     tribo_id int not null AUTO_INCREMENT,
@@ -15,7 +19,7 @@ CREATE TABLE squad(
 CREATE TABLE tripulantes_squad(
     matricula int NOT NULL AUTO_INCREMENT,
     nome varchar(50) NOT NULL,
-    tempo_empresa numeric(2,2) NOT NULL,
+    data_contratacao date NOT NULL,
     cargo varchar(50) NOT NULL,
     slack varchar(50) NOT NULL,
     squad_id int NOT NULL,
@@ -32,7 +36,7 @@ CREATE TABLE series(
 CREATE TABLE series_assistidas(
     serie_id int NOT NULL,
     matricula int NOT NULL,
-    assistiu TIMESTAMP WITH TIME ZONE,
+    assistiu date not null,
     PRIMARY KEY (serie_id, matricula),
     FOREIGN KEY (serie_id) REFERENCES series(serie_id),
     FOREIGN KEY (matricula) REFERENCES tripulantes_squad(matricula)
