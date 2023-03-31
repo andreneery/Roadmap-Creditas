b5f1bcf14af73fa5bec67503bf808b02570849d1
André Nery <andre.nery@creditas.com>
2023-03-31 10:25:25 -0300

    adicionando exercicios 6 e 8

--- src/main/kotlin/bancoDeDados/ERD/queries/queries.sql
+++ src/main/kotlin/bancoDeDados/ERD/queries/queries.sql
@@ -1,3 +1,5 @@
+USE TRIBE_EXERCISE;
+
 -- Exercicio 1
 -- Quantas pessoas com mais de 6 meses são desenvolvedoras na Creditas
 SELECT * FROM TRIBE_EXERCISE.tripulante
@@ -33,7 +35,6 @@ LIMIT 1;
 
 --EX 5.
 -- Qual a média de pessoas que as Squads tem
-USE TRIBE_EXERCISE;
 SELECT s.squad_name, AVG(count_tripulante)
 FROM (
   SELECT squad_id, COUNT(matricula) AS count_tripulante
@@ -42,4 +43,32 @@ FROM (
 ) AS contar_tripulantes
 INNER JOIN squad AS s ON contar_tripulantes.squad_id = s.squad_id
 GROUP BY s.squad_name;
+
+
+--EX 6
+-- Media de tempo de contratacao tem as squads
+SELECT
+	squad.squad_name,
+	CONCAT(FLOOR(AVG(DATEDIFF(NOW(), tripulante.data_contratacao) / 365)), ' anos e ',
+    MOD(FLOOR(AVG(DATEDIFF(NOW(), tripulante.data_contratacao) / 30)), 12), ' meses') as media_contratacao
+FROM squad
+INNER JOIN tripulante_squad ON squad.squad_id = tripulante_squad.squad_id
+INNER JOIN tripulante ON tripulante_squad.matricula = tripulante.matricula
+GROUP BY squad.squad_name;
+
+--ex 7
+-- Para cada Tribe, qual o total de pessoas trabalhando nela (Cuidado para não contas pessoas cross duas vezes)
+-- VOU DEIXAR POR ULTIMO
+
+
+-- ex 8
+-- Para cada Tribe, qual o total de pessoas trabalhando nela (
+SELECT
+	product_tech.tribo_name,
+	COUNT(tripulante.matricula)
+FROM product_tech
+INNER JOIN squad ON product_tech.tribo_id = squad.tribo_id
+INNER JOIN tripulante_squad ON squad.squad_id = tripulante_squad.squad_id
+INNER JOIN tripulante ON tripulante_squad.matricula = tripulante.matricula
+GROUP BY product_tech.tribo_name;
 
