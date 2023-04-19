09495c6b072db837c127f03a465920176d63750b
André Nery <andre.nery@creditas.com>
2023-04-19 10:17:10 -0300

    ajustando exercicio 3

--- src/main/kotlin/bancoDeDados/ERD/queries/queries.sql
+++ src/main/kotlin/bancoDeDados/ERD/queries/queries.sql
@@ -14,12 +14,7 @@ WHERE data_contratacao > date_sub(NOW(), INTERVAL 12 MONTH) AND cargo = "Product
 -- Buscar o funcionario mais antigo na tabela e mostrar o tempo de empresa
 SELECT
     nome,
-    CONCAT(
-        DATEDIFF(CURDATE(), data_contratacao) DIV 365,
-        ' anos e ',
-        DATEDIFF(CURDATE(), data_contratacao) MOD 365 DIV 30,
-        ' meses'
-    ) AS tempo_empresa
+        (DATEDIFF(CURDATE(), data_contratacao) DIV 30) / 12
     FROM TRIBE_EXERCISE.tripulante
 WHERE
     data_contratacao = (SELECT MIN(data_contratacao) FROM TRIBE_EXERCISE.tripulante)
